151c819a75152be3a8b31a6c645d3f430bfbc76c
dltnk9 <dltnk9@naver.com>
2022-08-22 15:15:56 +1000

    br tag fix <br/>

--- src/App.jsx
+++ src/App.jsx
@@ -21,16 +21,16 @@ function App() {
 
   <div className='App'>
     
-    <h1 className="title" > WCP User Database</h1><br></br><button className="subtitle" onClick={onClick}>Fetch 10 Users</button><br></br>
+    <h1 className="title" > WCP User Database</h1><br/><button className="subtitle" onClick={onClick}>Fetch 10 Users</button><br/>
     {user?.map((user) => (
       <div key={user.id} className="card">
         <div className="inner"> 
         <div className="front">
-      <div><img className="img" src={user.avatar}></img><br></br> {user.first_name}<br></br>{user.last_name}<p>{user.employment.title}</p></div>
+      <div><img className="img" src={user.avatar}></img><br/> {user.first_name}<br/>{user.last_name}<p>{user.employment.title}</p></div>
      
       </div>
       <div className="back">User Name: {user.username}<br></br><a href='mailto:MAIL_ADDRESS'>Email : {user.email}</a><br></br>Gender : {user.gender}<br></br>Phone Number : {user.phone_number}
-<br></br>Date of Birth : {user.date_of_birth}<br></br>Key Skill : {user.employment.key_skill}<br></br><div className="address"> Address : {user.address.city} {user.address.street_name} <br></br>{user.address.street_address} {user.address.zip_code} {user.address.state}<br></br>{user.address.country}</div></div>
+<br/>Date of Birth : {user.date_of_birth}<br/>Key Skill : {user.employment.key_skill}<br/><div className="address"> Address : {user.address.city} {user.address.street_name} <br/>{user.address.street_address} {user.address.zip_code} {user.address.state}<br/>{user.address.country}</div></div>
         </div>
         </div>
     ))}
